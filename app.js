cae99d7d6e5337ed44e764692f1be2055c257a59
soni-shashan <lumbhanishashan1510@gmail.com>
2025-08-26 10:27:21 +0530

    changes in api routes

--- app.js
+++ app.js
@@ -54,8 +54,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/', usersRouter);
-app.use('/api', contestRouter);
-app.use('/api', problemRouter);
+app.use('/api/contests', contestRouter);
+app.use('/api/problems', problemRouter);
 
 
 // catch 404 and forward to error handler
